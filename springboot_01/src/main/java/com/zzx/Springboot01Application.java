072db4354e3d7b3c5b4d92e32defd48784c2f030
zzx <906596387@qq.com>
2021-12-02 15:00:20 +0800

    12.2 3点

---
+++ springboot_01/src/main/java/com/zzx/Springboot01Application.java
@@ -0,0 +1,19 @@
+package com.zzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zzx
+ * @Date: 2021/11/29 - 11 - 29 - 14:42
+ * @Description: com.zzx
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class Springboot01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01Application.class,args);
+    }
+
+
+}
